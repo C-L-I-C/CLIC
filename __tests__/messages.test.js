e83167046c606e5af8e0c77445987e736b05ea8c
Denzel <dbartolaba123@gmail.com>
2022-04-14 15:50:34 -0700

    created a controller for message

--- __tests__/messages.test.js
+++ __tests__/messages.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Message = require('../lib/models/Message');
 
 describe('CLIC routes', () => {
   beforeEach(() => {
@@ -11,4 +12,18 @@ describe('CLIC routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should be able to insert and instance of message into messages', async () => {
+    const response = await Message.insert({
+      userId: '1',
+      message: 'You rock!'
+    });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      userId: '1',
+      message: 'You rock!',
+      createdAt: expect.any(String)
+  });
+  })
 });
