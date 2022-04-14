234881ea33162f95203faf43b7470b7669531b11
Alice Hsing <alicehsing@gmail.com>
2022-04-14 14:35:22 -0700

    setup sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,27 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT
+);
+
+CREATE TABLE messages (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    message VARCHAR(255) NOT NULL,
+    user_id BIGINT REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, email)
+VALUES 
+('user 1', 'user1@test.com'),
+('user 2', 'user@test.com');
+
+INSERT INTO messages (message, user_id)
+VALUES 
+('HELLO WORLD!', '1'),
+('Goodbye, see you next time!', '2');
