ba62140a1616b7b1fd2d6c30d7edf1672d76778f
Alice Hsing <alicehsing@gmail.com>
2022-04-19 12:42:01 -0700

    refactored print and create ascii code into a function handleAscii and cleaned up code

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ const io = require('socket.io-client');
 const socket = io('http://localhost:3000');
 // import readline to read from console
 const inquirer = require('inquirer');
-// inquirer.registerPrompt('selectLine', require('inquirer-select-line'));
+
 // create an interface to get input from the terminal console
 
 async function messagePrompt() {
@@ -16,64 +16,60 @@ async function messagePrompt() {
       name: 'message',
     })
     .then(async ({ message }) => {
-      // process.stdout.write('\r\x1b[K');
       socket.emit('server:message', message);
       await checkInput(message);
       messagePrompt();
     });
 }
 
+function handleAscii(prompt) {
+  return prompt.then((answer) => {
+    if (answer.operation === 'print') {
+      socket.emit('getList', 'ASCII');
+    } else if (answer.operation === 'create') {
+      const ascii = {};
+
+      return inquirer
+        .prompt({
+          type: 'input',
+          message: 'Name your ASCII!',
+          name: 'name',
+        })
+        .then((answer) => {
+          ascii.name = answer.name;
+          return inquirer
+            .prompt({
+              type: 'input',
+              message: 'Create your ASCII!',
+              name: 'string',
+            })
+            .then((answer) => {
+              ascii.string = answer.string;
+              socket.emit('create', ['ASCII', ascii]);
+            });
+        });
+    }
+  });
+}
+
 async function checkInput(text) {
   if (text.charAt(0) === '/') {
-    const prompt = inquirer
-      .prompt({
-        type: 'list',
-        message: 'Which operation would you like to choose?',
-        name: 'operation',
-        choices: ['print', 'create'],
-      });
+    const prompt = inquirer.prompt({
+      type: 'list',
+      message: 'Which operation would you like to choose?',
+      name: 'operation',
+      choices: ['print', 'create'],
+    });
 
     switch (text) {
       case '/ascii':
-        //put 38-66 in function
-        await prompt
-          .then((answer) => {
-            if (answer.operation === 'print') {
-              console.log('ANSWER EMITS TO SERVER?');
-              socket.emit('getList', 'ASCII');
-            } else if (answer.operation === 'create') {
-              const ascii = {};
-              //put 46-66 in function
-              return inquirer
-                .prompt({
-                  type: 'input',
-                  message: 'Name your ASCII!',
-                  name: 'name',
-                })
-                .then((answer) => {
-                  ascii.name = answer.name;
-                  return inquirer
-                    .prompt({
-                      type: 'input',
-                      message: 'Create your ASCII!',
-                      name: 'string',
-                    })
-                    .then((answer) => {
-                      ascii.string = answer.string;
-                      // console.log('Did it work?', ascii);
-                      socket.emit('create', ['ASCII', ascii]);
-                    });
-                });
-            }
-          });
+        await handleAscii(prompt);
     }
-  }
-  else {
+  } else {
     // send the user message to the socket server
     if (text.trim().length) socket.emit('server:message', text.trim());
   }
 }
-
 
 // Get the users name
 inquirer
@@ -92,12 +88,8 @@ inquirer
   })
   .catch(console.error);
 
-
-
-
 //Listen for event message from the server
 socket.on('client:message', (text) => {
-  // console.log('RECEIVED MESSAGE', text);
   // Erase last line > prompt
   // erases the current line in the console and rewrites something
   process.stdout.write('\r\x1b[K');
@@ -109,35 +101,19 @@ socket.on('client:message', (text) => {
 
 //Listen for ascii names being sent from server
 socket.on('selectList', async ([names, list]) => {
-  console.log('CLIENT SELECT LIST');
-  // erases the current line in the console and rewrites something
-  // process.stdout.write('\r\x1b[K');
+  const prompt = inquirer.prompt({
+    type: 'list',
+    message: 'Select ASCII',
+    name: 'select',
+    choices: names,
+  });
 
-  const prompt = inquirer
-    .prompt({
-      type: 'list',
-      message: 'Select ASCII',
-      name: 'select',
-      choices: names,
-    });
-
-  await prompt.then((answer) => {
-    // socket.emit('', )
-    const choice = list.find((entry) => {
-      return answer.select === entry.name;
-    });
-    // console.log(choice.string);
-    socket.emit('emitAscii', choice.string);
-  })
+  await prompt
+    .then((answer) => {
+      const choice = list.find((entry) => {
+        return answer.select === entry.name;
+      });
+      socket.emit('emitAscii', choice.string);
+    })
     .catch((error) => console.log(error));
-
-  // console log out arrow without doing a new line
-  // process.stdout.write('> ');
-  console.log('LINE 81 CLIENT');
-
 });
-
-//Prompt user to enter a message
-
-// When user inputs text, fire readline 'line' event which emits the message with socket.io
-
--- server.js
+++ server.js
@@ -32,7 +32,6 @@ io.on('connection', (socket) => {
 
   //if user emitted 'new user' event, this callback will be called
   socket.on('new user', async (name) => {
-    console.log('SERVER NEW USER');
     // THIS IS WHERE WE INSERT IN OUR USER MODEL?
     // THIS WHERE WE FETCH AND BROADCAST/EMIT PAST MESSAGES?
     //store users name
@@ -46,73 +45,54 @@ io.on('connection', (socket) => {
 
     chatHistory.map((entry) => {
       const chat = `${entry.userId} at ${entry.createdAt} said ${entry.message}`;
-
-      // console.log('chat', chat);
-
       socket.emit('client:message', chat);
     });
-
     // now we want to emit an event to all users except that user, that the new user has joined the chat
     socket.broadcast.emit('client:message', `${name} joined the chat.`);
   });
 
   // Listen for a message event
   socket.on('server:message', async (text) => {
-    console.log('SERVER MESSAGE');
     // THIS IS WHERE WE INSERT IN OUT MESSAGE MODEL?
     await Message.insert({
       message: text,
       // userId: `${users[socket.id]}`,
     });
     // emit an event to all users except that user
-    console.log('what is this', text, text.length);
-
     socket.broadcast.emit('client:message', `${users[socket.id]}: ${text}`);
-
   });
 
   socket.on('emitAscii', async (text) => {
-    console.log('SERVER EMIT ASCII');
     // THIS IS WHERE WE INSERT IN OUT MESSAGE MODEL?
     await Message.insert({
       message: text,
       // userId: `${users[socket.id]}`,
     });
     // emit an event to all users except that user
-    console.log('what is this', text, text.length);
     io.emit('client:message', `${users[socket.id]}: ${text}`);
-
   });
-
-
 
   //listen for /getList command
   socket.on('getList', async (command) => {
-    console.log('SERVER GET LIST');
     const cmd = {
       ASCII,
     };
     //fetch out asciiNames from our DB
-    // console.log('Commands!!!', command);
     const list = await cmd[command].getAll();
     //map through the array of object and broadcast the names back?
     const names = [];
     list.map((object) => {
       names.push(object.name);
-      // console.log('names?', object.name);
     });
-    // console.log('LISTSSSS', list);
     socket.emit('selectList', [names, list]);
   });
 
   socket.on('create', async ([command, object]) => {
-    console.log('SERVER CREATE ASCII');
-    // console.log('OBJECTTTT', object);
     const cmd = {
       ASCII,
     };
     const create = await cmd[command].insert(object);
-    // console.log('CREATE', create);
+
     if (create) {
       socket.emit('client:message', 'A new ASCII has been created!');
     } else {
