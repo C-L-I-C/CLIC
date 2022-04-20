20ae40123367aa71eabaefad8f76436abd33a059
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-20 12:22:21 -0700

    controller route post /sessions, model Admin functions findByEmail, authToken, passwordHash, AdminService function signIn written and test passing.

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const AdminService = require('../lib/services/AdminService');
 
 describe('Admin routes', () => {
   beforeEach(() => {
@@ -21,6 +22,23 @@ describe('Admin routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email: 'user@admin.com',
+    });
+  });
+
+  it('should be able to sign in an existing Admin', async () => {
+    const admin = await AdminService.create({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    const res = await request(app).post('/api/v1/admins/sessions').send({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    expect(res.body).toEqual({
+      message: 'Signed in successfully!',
+      admin,
     });
   });
 });
