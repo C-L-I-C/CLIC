8cd733501ee585c72a60c602c20dc9055e2b84fe
Clayton Knapp <csjknapp@gmail.com>
2022-04-20 15:45:35 -0700

    tried new socketio express server together for deploy test

--- server.js
+++ server.js
@@ -2,32 +2,43 @@ const app = require('./lib/app');
 const Emoticon = require('./lib/models/Emoticon');
 const Message = require('./lib/models/Message');
 const User = require('./lib/models/User');
-const pool = require('./lib/utils/pool');
-const express = require('express');
-const socketIO = require('socket.io');
+// const pool = require('./lib/utils/pool');
+// const express = require('express');
+const { createServer } = require('http');
+// const socketIO = require('socket.io');
+const { Server } = require('socket.io');
 
-const API_URL = process.env.API_URL || 'http://localhost';
+// const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
+// const SOCKET_PORT = process.env.SOCKET_PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀  Server started on ${API_URL}:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀  Server started on ${API_URL}:${PORT}`);
+// });
 
-process.on('exit', () => {
-  console.log('👋  Goodbye!');
-  pool.end();
-});
+// process.on('exit', () => {
+//   console.log('👋  Goodbye!');
+//   pool.end();
+// });
+
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+httpServer.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 //HTTP SERVER FOR SOCKET IO PER HEROKU DOCS
-const SOCKET_PORT = process.env.SOCKET_PORT || 3000;
-const CLIENT = '/client.js';
+// const SOCKET_PORT = process.env.SOCKET_PORT || 3000;
+// const CLIENT = '/client.js';
 
-const server = express()
-  .use((req, res) => res.sendFile(CLIENT, { root: __dirname }))
-  .listen(SOCKET_PORT, () => console.log(`Listening on ${SOCKET_PORT}`));
+// const server = express()
+//   .use((req, res) => res.sendFile(CLIENT, { root: __dirname }))
+//   .listen(SOCKET_PORT, () => console.log(`Listening on ${SOCKET_PORT}`));
 
-const io = socketIO(server);
+// const io = socketIO(server);
+
 
 
 
