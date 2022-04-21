96b883291069be52cdb267667443e30674d77149
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-20 22:03:53 -0700

    controller admins route get /user/:id, model User function getById written and test passing.

--- __tests__/admin-users.test.js
+++ __tests__/admin-users.test.js
@@ -15,7 +15,7 @@ describe('CLIC routes', () => {
     pool.end();
   });
 
-  it('should be able to get each instance of User from users', async () => {
+  it('should be able to get each instance of User from users if authenticated', async () => {
     const agent = request.agent(app);
 
     await AdminService.create({
@@ -34,5 +34,27 @@ describe('CLIC routes', () => {
       { id: expect.any(String), email: 'user1@test.com', username: 'user 1' },
       { id: expect.any(String), email: 'user2@test.com', username: 'user 2' },
     ]);
+  });
+
+  it('should be able to get an instance of User from users by id if authenticated', async () => {
+    const agent = request.agent(app);
+
+    await AdminService.create({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    await agent.post('/api/v1/admin/session').send({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    const res = await agent.get('/api/v1/admin/user/1');
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'user1@test.com',
+      username: 'user 1',
+    });
   });
 });
