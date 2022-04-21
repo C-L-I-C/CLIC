7a36ce7c672c6537198b8972fd1b61d6a917928e
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-20 21:57:57 -0700

    controller admin route get /users written and test passing.

---
+++ __tests__/admin-users.test.js
@@ -0,0 +1,38 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const AdminService = require('../lib/services/AdminService');
+
+const User = require('../lib/models/User');
+
+describe('CLIC routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should be able to get each instance of User from users', async () => {
+    const agent = request.agent(app);
+
+    await AdminService.create({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    await agent.post('/api/v1/admin/session').send({
+      email: 'user@admin.com',
+      password: 'whatever',
+    });
+
+    const res = await agent.get('/api/v1/admin/user');
+
+    expect(res.body).toEqual([
+      { id: expect.any(String), email: 'user1@test.com', username: 'user 1' },
+      { id: expect.any(String), email: 'user2@test.com', username: 'user 2' },
+    ]);
+  });
+});
